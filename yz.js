3d38cfa94325ddad0c9a0c75bdfb437451929591
东方渣客 <67358258+skyfly410@users.noreply.github.com>
2024-08-24 18:37:59 +0800

    yz.js

--- yz.js
+++ yz.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var token = new URLSearchParams(window.location.search).get('token');
     if (!token || !isValidToken(token)) {
         alert('无效的访问令牌，请从登录页面访问。');
-        window.location.href = 'https://skyfly410.github.io/bmfile.html'; // 重定向到登录页面
+        window.location.href = 'https://skyfly410.github.io/bmfile-bmpic.html'; // 重定向到登录页面
     }
 });
 
