af38ac6e8fafdbb6fa878aa12f301e76a168cd10
东方渣客 <67358258+skyfly410@users.noreply.github.com>
2024-08-24 17:52:00 +0800

    szpw.js

--- szpw.js
+++ szpw.js
@@ -2,8 +2,8 @@ function validateAndRedirect() {
     var password = document.getElementById('password').value;
     var correctPassword = "2012"; 
     if (password === correctPassword) {
-        window.location.href = 'bmfile-bmpic.html'; 
-        return true;
+        window.location.href = '/skyfly410/skyfly410.github.io/edit/master/bmfile-bmpic.html';//
+        return false;
     } else {
         alert('输入错误，请重试！');
         // var messageElement = document.getElementById('message');
