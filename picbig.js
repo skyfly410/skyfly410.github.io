12bb3031aa91f4debbf3e3fec681d91e9c0202b6
东方渣客 <67358258+skyfly410@users.noreply.github.com>
2024-08-25 17:11:28 +0800

    picbig.js

--- picbig.js
+++ picbig.js
@@ -11,8 +11,8 @@ var images = document.querySelectorAll('.image-container .image');
 // 点击图片时执行的函数
 function openModal(event) {
   modal.style.display = "block";
-  img.src = this.src;
-  captionText.innerHTML = this.alt;
+  modalImg.src = img.src;
+  caption.innerHTML = img.alt;
 }
 
 // 点击关闭按钮时执行的函数
