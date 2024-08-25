76b35337183e6ec26b93f9674b68062807305473
东方渣客 <67358258+skyfly410@users.noreply.github.com>
2024-08-25 10:28:43 +0800

    picbig.js

--- picbig.js
+++ picbig.js
@@ -29,10 +29,7 @@ window.onclick = function(event) {
 
 // 为每个图片添加点击事件
 images.forEach(function(image) {
-  image.addEventListener('click', function() {
-// 添加边框线样式
-    this.classList.add('border-active');
-  });
+  image.onclick = openModal;
 });
 
 // 关闭按钮的点击事件
