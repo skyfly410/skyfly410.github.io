dfa8847f8754397770559a7f2b0cb5a36a827ebc
东方渣客 <67358258+skyfly410@users.noreply.github.com>
2024-08-25 09:44:17 +0800

    szpw.js

--- szpw.js
+++ szpw.js
@@ -2,7 +2,8 @@ function validateAndRedirect() {
     var password = document.getElementById('password').value;
     var correctPassword = "2012"; 
     if (password === correctPassword) {
-        window.location.href = 'https://skyfly410.github.io/bmfile-bmpic=2012.html';//
+        sessionStorage.setItem('passwordVerified', 'true');
+        window.location.href = 'https://skyfly410.github.io/bmfile-bmpic.html';//
         return false;
     } else {
         alert('请认真输入，要不然我就不给你看了，满足不了你的好奇心了！');
